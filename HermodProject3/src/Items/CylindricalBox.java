305effc82216a277ac5b3a4d8a357fe14adcccf9
Samuel2800 <84688297+Samuel2800@users.noreply.github.com>
2022-06-25 17:02:50 +0100

    Update CylindricalBox.java

--- HermodProject3/src/Items/CylindricalBox.java
+++ HermodProject3/src/Items/CylindricalBox.java
@@ -1,9 +1,8 @@
 //Daniel Ramos
 //Gustavo Matamoros
-//Carlos Peñafiel
+//Carlos PeÃ±afiel
 //Samuel Pantoja
 package Items;
-//I found out I misspelled "Cylindrical" way to far into the code and have no intention of changing it
 public class CylindricalBox extends item {
 	public CylindricalBox(String itemName, int amount, double weight, double x, double y, double height, double volume, String baseShape) {
 		super(itemName, amount, weight, x, y, height, volume, baseShape);
